eabd1702dd4f8788573a6f41fe1b71aacdb2fc3b
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-05-16 01:58:08 +0600

    added project2

---
+++ project2.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main()
+{
+    printf("Hello i'm nahid\nI'm from Bangladesh \nI'm learning C language \n& It's awesome\nAre you interested in learning C language?\nYes\\No");
+}
