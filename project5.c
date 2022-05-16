f5d0a51aad7d86a2722b736375e102c92b00f536
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-05-16 23:07:43 +0600

    const value

---
+++ project5.c
@@ -0,0 +1,11 @@
+/* const value */
+#include <stdio.h>
+
+int main()
+{
+    const int minutesPerHour = 60;
+    const float PI = 3.14159126;
+    printf("%d\n", minutesPerHour);
+    printf("%.8f\n", PI);
+    return 0;
+}
---
+++ project5_1.c
@@ -0,0 +1,9 @@
+/* const value */
+#include <stdio.h>
+int main()
+{
+    const int value1 = 35;
+    const int value2 = 35;
+    const int value3 = value1 + value2;
+    printf("%d", value3);
+}
