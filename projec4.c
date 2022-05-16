3efa143cfe4f71695c4ab7b24ec899fd685f6a42
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-05-16 21:00:07 +0600

    added variables type

---
+++ projec4.c
@@ -0,0 +1,15 @@
+/* C variables */
+#include <stdio.h>
+
+int main() {
+  // Create variables
+  int myNum = 5;                        // Integer (whole number)
+  float myFloatNum = 5.9956894854;     // Floating point number
+  char myLetter = 'D';                // Character
+  
+  // Print variables
+  printf("%d\n", myNum);
+  printf("%.3f\n", myFloatNum);
+  printf("%c\n", myLetter);
+  return 0;
+}
