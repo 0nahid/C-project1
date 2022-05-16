84a60b1f4d571828bb19eca5e7a21fae7120c536
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-05-16 20:38:07 +0600

    comment in c

---
+++ project3.c
@@ -0,0 +1,10 @@
+/*comment in c */
+#include <stdio.h>
+
+int main()
+{
+    /* The code below will print the words Hello World!
+    to the screen, and it is amazing */
+    printf("Hello World!");
+    return 0;
+}
