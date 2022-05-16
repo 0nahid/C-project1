9092a2368307e5a01dfd8b1316877b19a6798a76
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-05-16 22:53:58 +0600

    variable prac

---
+++ project4_1.c
@@ -0,0 +1,12 @@
+/* variables */
+#include <stdio.h>
+int main()
+{
+    char letter = 'D';
+    printf("%c\n", letter);
+    int num1 = 65536;
+    printf("%d\n", num1);
+    float num2 = 5947895.254;
+    printf("%f\n", num2);
+    
+}
---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+    int num1 = 65536;
+    printf("%d\n", num1);
+    float num2 = 5947895.254;
+    printf("%f", num2);
