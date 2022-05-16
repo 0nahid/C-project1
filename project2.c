658480445bff46c66c78746b641e725fc296c793
Nahid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-05-16 20:35:08 +0600

    added use of some symbol

--- project2.c
+++ project2.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello i'm nahid\nI'm from Bangladesh \nI'm learning C language \n& It's awesome\nAre you interested in learning C language?\nYes\\No");
+    // to print " " in output just use \" \"
+    // to print a line break use \n
+    // to print output with a tab value use \t
+    // to print backshash use double slash (\\) in code
+    printf("Hello i'm \"Nahid\"\nI'm from Bangladesh \nI'm learning C language \n& It's awesome\nAre you interested in learning C language?\nYes\\No");
 }
