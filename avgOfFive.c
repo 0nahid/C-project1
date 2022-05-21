afa67d51fc1ae4a7c25f49c1b9175cd88f3298b8
Nahid Hassan Bulbul <41489906+0nahid@users.noreply.github.com>
2022-05-21 10:32:08 +0600

    avgofFive

---
+++ avgOfFive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+    int a,b,c,d,e,m;
+    float avg;
+    printf("Enter five numbers :\n");
+    scanf("%d%d%d%d%d", &a, &b, &c , &d, &e);
+    avg = (float)(a+b+c+d+e)/5 ;
+    printf("Average of the  %f\n", avg);
+    m=(int)avg;
+    printf("Integral part is : %d" , m);
+
+}
