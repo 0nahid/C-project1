f2d3a55bfc5baf89d7c8e91fdc9c23874728f9fb
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-05-25 03:33:52 +0600

    added auto clear console but shows a error

--- grade.c
+++ grade.c
@@ -4,13 +4,11 @@
 int main()
 {
 gradeCheck:
-   
     int obtainedMarks;
     printf("Enter your number :\n");
     scanf("%d", &obtainedMarks);
     (obtainedMarks >= 0 & obtainedMarks <= 100) ? (obtainedMarks >=90)?(printf("A grade\n")):(obtainedMarks >= 87)?(printf("B+ grade\n")):(obtainedMarks >=84)?(printf("B grade\n")):(obtainedMarks >= 80)?(printf("B- grade\n")):(obtainedMarks >= 77)?(printf("C+ grade\n")):(obtainedMarks >=74)?(printf("C grade")):(obtainedMarks >= 70)?(printf("C- grade\n")):(obtainedMarks >= 65)?(printf("D+ grade\n")):(obtainedMarks >=60)?(printf("D grade")):printf("F grade") : printf("Invalid number\n");
     printf("\n");
-    // system("clear");
     goto gradeCheck;
     return 0;
 }
