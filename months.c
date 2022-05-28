4250812ba3db62ea2b711534a4590ec43d29e221
Nahid Hassan Bulbul <41489906+0nahid@users.noreply.github.com>
2022-05-28 10:22:09 +0600

    changes the logic

--- months.c
+++ months.c
@@ -5,6 +5,7 @@ int main()
     printf("Enter days to check the months & dates :\n");\
     scanf("%d",&dates);
     months = dates / 30 ;
-    days = dates - (months*30);
+    // days = dates - (months*30);
+    days = dates % 30;
     printf("The result is months: %d & dates is %d",months,days);
 }
