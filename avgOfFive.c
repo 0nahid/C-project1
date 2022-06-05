fe5521342d0f0225d5077c830bc3f59cb7e06e6e
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-05 23:54:24 +0600

    average of five numbers

--- avgOfFive.c
+++ avgOfFive.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
 main()
 {
-    int a,b,c,d,e,m;
+    int a, b, c, d, e, m;
     float avg;
     printf("Enter five numbers :\n");
-    scanf("%d%d%d%d%d", &a, &b, &c , &d, &e);
-    avg = (float)(a+b+c+d+e)/5 ;
+    scanf("%d%d%d%d%d", &a, &b, &c, &d, &e);
+    avg = (float)(a + b + c + d + e) / 5;
     printf("Average of the  %f\n", avg);
-    m=(int)avg;
-    printf("Integral part is : %d\n" , m);
-
+    m = (int)avg;
+    printf("Integral part is : %d\n", m);
+    (int)avg % 3 == 0 ? printf("Average is divisible by 3\n") : printf("Average is not divisible by 3\n");
 }
