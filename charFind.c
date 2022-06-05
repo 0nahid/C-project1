a363761b2e882f14582b8529118d25fdda4ac1c1
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-05 23:22:43 +0600

    findout charecter

--- charFind.c
+++ charFind.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+int main()
 {
-  char m;
-  scanf("%c",&m);
-  ((m>="A" && m <="Z")||((m>="a" && m <="b")))?(printf("Alphabet"))
-  :((m>="0" && m<="9")?(printf("Numeric")):(printf("Special")));
+  char c;
+  printf("Enter a character: ");
+  scanf("%c", &c);
+
+  (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') ? 
+  (printf("%c is an alphabet\n", c)) : 
+  (c >= '0' && c <= '9') ? (printf("%c is a digit\n", c)) : 
+  (printf("%c is a special character\n", c));
+
+  return 0;
 }
