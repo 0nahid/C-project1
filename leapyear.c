6695d706448f2ce5f4cc292334a7a85f1fd2c61b
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-06 02:25:01 +0600

    leap year

--- leapyear.c
+++ leapyear.c
@@ -1,9 +1,8 @@
-  #include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int year;
-    printf("Enter a year to check it is a leap year or not: \n");
-    scanf("%d",&year);
-    printf("%d ",year);
-    (year % 4 == 0 && year / 100 != 0 || year % 400 == 0 ) ? (printf("is a Leap year")) : (printf("is not a leap year"));
+  int year;
+  printf("Enter a year to check it is a leap year or not: \n");
+  scanf("%d", &year);
+  ( (year % 4 == 0 && year / 100 != 0) || (year % 400 == 0 )) ? (printf("%d is a Leap year\n", year)) : (printf("%d is not a leap year\n", year));
 }
