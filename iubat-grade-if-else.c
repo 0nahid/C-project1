c4ab0e1e858eba6da06a2fe339a31cbbb01f6a22
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-12 00:46:48 +0600

    compress v1

--- iubat-grade-if-else.c
+++ iubat-grade-if-else.c
@@ -5,61 +5,46 @@ recheck:
     int marks;
     printf("Enter your marks:\n");
     scanf("%d", &marks);
-    if (marks >= 0 && marks <= 100)
-    {
-        if (marks > 90)
-        {
+    if (marks >= 0 && marks <= 100){
+        if (marks > 90){
             printf("A+\n");
         }
-        if (marks < 90)
-        {
-            if (marks >= 87)
-            {
+        if (marks < 90){
+            if (marks >= 87){
                 printf("B+\n");
             }
-            else if (marks >= 84)
-            {
+            else if (marks >= 84){
                 printf("B\n");
             }
-            else if (marks >= 80)
-            {
+            else if (marks >= 80){
                 printf("B-\n");
             }
         }
-        if (marks < 80)
-        {
-            if (marks >= 77)
-            {
+        if (marks < 80){
+            if (marks >= 77){
                 printf("C+\n");
             }
-            else if (marks >= 74)
-            {
+            else if (marks >= 74){
                 printf("C\n");
             }
-            else if (marks >= 70)
-            {
+            else if (marks >= 70){
                 printf("C-\n");
             }
         }
 
-        if (marks < 70)
-        {
-            if (marks >= 65)
-            {
+        if (marks < 70){
+            if (marks >= 65){
                 printf("D+\n");
             }
-            else if (marks >= 60)
-            {
+            else if (marks >= 60){
                 printf("D\n");
             }
         }
-        if (marks < 60)
-        {
+        if (marks < 60){
             printf("You've failed for %d marks\n", 60 - marks);
         }
     }
-    else
-    {
+    else{
         printf("Invalid marks\n");
     }
     printf("\n");
