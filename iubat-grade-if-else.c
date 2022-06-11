9a887605daf2d159abec52b7245aae3a4294744d
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-06-11 23:17:23 +0600

    fixed: iubat grade with if else

--- iubat-grade-if-else.c
+++ iubat-grade-if-else.c
@@ -6,37 +6,53 @@ int main()
     scanf("%d", &marks);
     if (marks >= 0 && marks <= 100)
     {
-        if (marks >= 90)
+        if (marks > 90)
         {
-            if(marks >90){
-                printf("A+\n");
-            }
-            else if (marks >=87){
+            printf("A+\n");
+        }
+        if (marks < 90)
+        {
+            if (marks >= 87)
+            {
                 printf("B+\n");
             }
-            else if (marks >=84){
+            else if (marks >= 84)
+            {
                 printf("B\n");
             }
-            else if (marks >=80){
+            else if (marks >= 80)
+            {
                 printf("B-\n");
             }
         }
-        else if (marks >=77){
-            printf("C+\n");
+        if (marks < 80)
+        {
+            if (marks >= 77)
+            {
+                printf("C+\n");
+            }
+            else if (marks >= 74)
+            {
+                printf("C\n");
+            }
+            else if (marks >= 70)
+            {
+                printf("C-\n");
+            }
         }
-        else if (marks >= 74){
-            printf("C\n");
+
+        if (marks < 70)
+        {
+            if (marks >= 65)
+            {
+                printf("D+\n");
+            }
+            else if (marks >= 60)
+            {
+                printf("D\n");
+            }
         }
-        else if(marks >= 70){
-            printf("C-\n");
-        }
-        else if(marks >= 65){
-            printf("D+\n");
-        }
-        else if(marks >= 60){
-            printf("D\n");
-        }
-        else
+        if(marks < 60)
         {
             printf("F\n");
         }
