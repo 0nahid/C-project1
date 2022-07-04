bf1b4fa656b7a675154a88a8b98feb39c5a5d4ab
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-07-05 01:38:53 +0600

    added strong number

---
+++ strongNumber.c
@@ -0,0 +1,28 @@
+// strong number
+#include <stdio.h>
+int main()
+{
+    int num, sum = 0, rem, temp, fact, i;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    temp = num;
+    while (temp != 0)
+    {
+        rem = temp % 10;
+        fact = 1;
+        for (int i = 1; i <= rem; i++)
+        {
+            fact = fact * i;
+        }
+        sum = sum + fact;
+        temp = temp / 10;
+    }
+    if (sum == num)
+    {
+        printf("%d is a strong number\n", num);
+    }
+    else
+    {
+        printf("%d is not a strong number\n", num);
+    }
+}
